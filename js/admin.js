b48a5c8f9ff7e189019eee1b20556b2cdf5bd6d0
Julius Czar <juliusczar.jc@gmail.com>
2019-01-29 19:47:35 +0100

    [feature #163568208] made the active navigation bar dynamic

--- js/admin.js
+++ js/admin.js
@@ -18,3 +18,18 @@ document.getElementById('createOffice').addEventListener('click', function(){
     document.querySelector('.create-party-cover').style.display = 'none';
     document.querySelector('.edit-party-cover').style.display = 'none';
 });
+
+ // Get the container element
+ var btnContainer = document.getElementById("user-sider-action");
+
+ // Get all buttons with class="btn" inside the container
+ var btns = btnContainer.getElementsByClassName("side");
+ 
+ // Loop through the buttons and add the active class to the current/clicked button
+ for (var i = 0; i < btns.length; i++) {
+   btns[i].addEventListener("click", function() {
+     var current = document.getElementsByClassName("active");
+     current[0].className = current[0].className.replace(" active", "");
+     this.className += " active";
+   });
+ }
--- js/user.js
+++ js/user.js
@@ -1,3 +1,21 @@
+
+   // Get the container element
+   var btnContainer = document.getElementById("user-sider-action");
+
+   // Get all buttons with class="btn" inside the container
+   var btns = btnContainer.getElementsByClassName("side");
+   
+   // Loop through the buttons and add the active class to the current/clicked button
+   for (var i = 0; i < btns.length; i++) {
+     btns[i].addEventListener("click", function() {
+       var current = document.getElementsByClassName("active");
+       current[0].className = current[0].className.replace(" active", "");
+       this.className += " active";
+     });
+   }
+   
+
+
 /***********User CONTROLS**********/
 document.getElementById('castVote').addEventListener('click', function(){
     document.querySelector('.party-container').style.display = 'none';
@@ -44,3 +62,7 @@ document.getElementById('expressInterest').addEventListener('click', function(){
     document.querySelector('.select-vote-box').style.display = 'none';
     document.querySelector('.interest-container').style.display = 'block';
 });
+
+
+
+
