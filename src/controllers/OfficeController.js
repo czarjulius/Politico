6786102f4623060660c364f8070211e4c58d884b
Julius Czar <juliusczar.jc@gmail.com>
2019-02-01 04:11:29 +0100

    feat(createOffice): admin can create office [Delivers #163569923]clear

--- src/controllers/OfficeController.js
+++ src/controllers/OfficeController.js
@@ -3,7 +3,18 @@ import offices from '../models/offices';
 
 class OfficeController {
 
-
+    static getOffice(req, res) {
+        const office = offices.find(c => c.id === parseInt(req.params.id, 10));
+        if (!office) {return res.status(404).json({
+          message: 'The office with the given ID not found.',
+        });}
+    
+        res.status(201).json({
+          message: 'Office successfully fetched',
+          office,
+        });
+      }
+    
   
   static postoffice(req, res) {
     const { type, name } = req.body;
