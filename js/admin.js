f710b3c7ecba8b8895f2c595a149e36e1a721ba9
Julius Czar <juliusczar.jc@gmail.com>
2019-02-05 22:10:15 +0100

    feat(editParty): Added edit party page - Added a form to handle office edit - Added a form to list offices
    [Delivers #163738790]

--- js/admin.js
+++ js/admin.js
@@ -4,21 +4,174 @@ document.getElementById('createParty').addEventListener('click', function(){
     document.querySelector('.create-office-cover').style.display = 'none';
     document.querySelector('.create-party-cover').style.display = 'block';
     document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+
 });
 
 document.getElementById('manageParty').addEventListener('click', function(){
     document.querySelector('.create-office-cover').style.display = 'none';
     document.querySelector('.create-party-cover').style.display = 'none';
     document.querySelector('.edit-party-cover').style.display = 'block';
-
+    document.querySelector('.edit-office-cover').style.display = 'none';
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
 
 });
 document.getElementById('createOffice').addEventListener('click', function(){
     document.querySelector('.create-office-cover').style.display = 'block';
     document.querySelector('.create-party-cover').style.display = 'none';
     document.querySelector('.edit-party-cover').style.display = 'none';
-});
+    document.querySelector('.edit-office-cover').style.display = 'none';
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
 
+});
+document.getElementById('editOffice').addEventListener('click', function(){
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'block';
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+
+});
+document.querySelector('.modalEdit1').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none'; 
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+   
+});
+document.querySelector('.modalEdit2').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';   
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+ 
+});
+document.querySelector('.modalEdit3').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none'; 
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+   
+});
+document.querySelector('.modalEdit4').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none'; 
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+   
+});
+document.querySelector('.modalEdit5').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none'; 
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+   
+});
+document.querySelector('.modalEdit6').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+  
+});
+document.querySelector('.modalEdit7').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none'; 
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+   
+});
+document.querySelector('.modalEdit8').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'block';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'none';
+  
+});
+document.querySelector('.officeEdit1').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit2').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit3').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit4').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit5').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit6').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
+document.querySelector('.officeEdit7').addEventListener('click', function(){
+    document.querySelector('.manageEdit-party-cover').style.display = 'none';
+    document.querySelector('.create-office-cover').style.display = 'none';
+    document.querySelector('.create-party-cover').style.display = 'none';
+    document.querySelector('.edit-party-cover').style.display = 'none';
+    document.querySelector('.edit-office-cover').style.display = 'none';  
+    document.querySelector('.manageEdit-office-cover').style.display = 'block';
+  
+});
  // Get the container element
  var btnContainer = document.getElementById("user-sider-action");
 
