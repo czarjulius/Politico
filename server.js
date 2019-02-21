c70f9d01e35b1d5a14eac55e7b00e659ea251040
czarjulius <juliusczar.jc@gmail.com>
2019-02-21 22:03:34 +0100

    installed cors

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import router from './server/routes/index';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
 
 app.get('/', (req, res) => {
   res.send(' Julius Welcome\'s you to Politico');
