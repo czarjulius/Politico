6535af0c676ae23175e87ddcf77f413d685e5cd6
czarjulius <juliusczar.jc@gmail.com>
2019-02-21 21:33:28 +0100

    Added Cors

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send(' Julius Welcome\'s you to Politico');
+});
+
 app.use('*', (req, res) => res.status(404).json({
   status: 'failed',
   message: 'route not found',
 }));
 
-app.get('/', (req, res) => {
-  res.send(' Julius Welcome\'s you to Politico');
-});
 
 const port = process.env.PORT || 8000;
 
